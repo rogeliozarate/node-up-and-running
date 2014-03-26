230ced7f9a43df05794dab2d50195685f9de4d32
Rogelio Zarate <i.am@rogeliozarate.com>
2014-03-26 11:32:44 -0600

    Code third iteraton of chat server
    - I am very excited

--- basic-chat-server.js
+++ basic-chat-server.js
@@ -1,14 +1,15 @@
 // Basic chat server
-//Second iteration	
+// Third iteration	
 
 
 var net = require('net')
-var chatServer = net.createServer()
-
+var chatServer = net.createServer(), clientList = []
 chatServer.on('connection', function(client) { client.write('Hi!\n');
-client.on('data', function(data) {
-console.log(data) })
-})
+clientList.push(client)
+client.on('data', function(data) { for(var i=0;i<clientList.length;i+=1) {
+//write this data to all clients
+clientList[i].write(data) }
+}) })
 chatServer.listen(9000)
 
 console.log('Server running at http://127.0.0.1:9000/');
