7f61586fd89f060b80a5ab71acd6aba7efbe6e59
Rogelio Zarate <i.am@rogeliozarate.com>
2014-03-26 11:30:03 -0600

    Code basic chat server

--- basic-chat-server.js
+++ basic-chat-server.js
@@ -1,10 +1,14 @@
 // Basic chat server
+//Second iteration	
+
 
 var net = require('net')
 var chatServer = net.createServer()
-chatServer.on('connection', function(client) { client.write('Hi!\n'); client.write('Bye!\n');
-client.end() })
+
+chatServer.on('connection', function(client) { client.write('Hi!\n');
+client.on('data', function(data) {
+console.log(data) })
+})
 chatServer.listen(9000)
 
 console.log('Server running at http://127.0.0.1:9000/');
-
